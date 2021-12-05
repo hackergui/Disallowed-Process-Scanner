8e76a69e6277a8677048698860455a7914317acd
hackergui <77847971+hackergui@users.noreply.github.com>
2021-12-05 16:03:49 +0300

    Add files via upload

---
+++ IllegalProcessScanner/IllegalProcessScanner/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IllegalProcessScanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Checker.Start();
+        }
+
+        private void Checker_Tick(object sender, EventArgs e)
+        {
+            Check("dnSpy"); // You can add another processes
+        }
+
+        private void Check(String procName)
+        {
+            Process[] processes = Process.GetProcessesByName(procName);
+            foreach (Process name in processes)
+            {
+                if (name.ProcessName.Contains(procName))
+                {
+                    name.Kill();
+                    MessageBox.Show("Yakaladım !");
+                    Process.Start("https://www.youtube.com/channel/UCA9NKzZym6coY-XFyYTw9fQ");
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+    }
+}
